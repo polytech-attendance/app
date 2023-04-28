ed7fcf30a2c24b6862389a13b14e77f65b343e21
Александр <timofeev2.as@edu.spbstu.ru>
2023-04-28 17:35:49 +0300

    [w\o] new method to pulling subject for groups

--- api/attendance_site/attendance/management/commands/update_subjects.py
+++ api/attendance_site/attendance/management/commands/update_subjects.py
@@ -12,37 +12,39 @@ from .make_new_user import make_new_user
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+
         # Получить данные из стороннего API
-        group = Group.objects.get(groupname="3530201/10001")
-        items = get_subject_cur_week(group.group_id)
+        groups = Group.objects.all()
+        for group in groups:
+            items = get_subject_cur_week(group.group_id)
 
-        # Добавление предметов в базу данных
-        subject_manager = Subject.objects
+            # Добавление предметов в базу данных
+            subject_manager = Subject.objects
 
-        for item in items:
-            if(item['teacher_id'] is None):
-                self.stdout.write('Teacher\'s id is None. SKIP\n')
-                continue
+            for item in items:
+                if(item['teacher_id'] is None):
+                    self.stdout.write('Teacher\'s id is None. SKIP\n')
+                    continue
 
-            try:
+                try:
+                    teacher = Teacher.objects.get(teacher_id=item['teacher_id'])
+                except Teacher.DoesNotExist:
+                    teacher_data = get_teacher_by_id(item['teacher_id'])
+                    teacher_user = make_new_user(teacher_data['first_name'])
+                    Teacher.objects.update_or_create(
+                        teacher_id=teacher_data['id'],
+                        user=teacher_user,
+                        teacher_name=teacher_data['full_name'],
+                    )
+                    self.stdout.write(self.style.SUCCESS(f'Teacher with name "{teacher_data["full_name"]}" created'))
                 teacher = Teacher.objects.get(teacher_id=item['teacher_id'])
-            except Teacher.DoesNotExist:
-                teacher_data = get_teacher_by_id(item['teacher_id'])
-                teacher_user = make_new_user(teacher_data['first_name'])
-                Teacher.objects.update_or_create(
-                    teacher_id=teacher_data['id'],
-                    user=teacher_user,
-                    teacher_name=teacher_data['full_name'],
+                subject, created = subject_manager.update_or_create(
+                    group=group,
+                    teacher=teacher,
+                    subject_name=item['subject_name']
                 )
-                self.stdout.write(self.style.SUCCESS(f'Teacher with name "{teacher_data["full_name"]}" created'))
-            teacher = Teacher.objects.get(teacher_id=item['teacher_id'])
-            subject, created = subject_manager.update_or_create(
-                group=group,
-                teacher=teacher,
-                subject_name=item['subject_name']
-            )
 
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Subject "{subject.subject_name}" created'))
-            else:
-                self.stdout.write(self.style.SUCCESS(f'Subject "{subject.subject_name}" updated'))
+                if created:
+                    self.stdout.write(self.style.SUCCESS(f'Subject "{subject.subject_name}" created'))
+                else:
+                    self.stdout.write(self.style.SUCCESS(f'Subject "{subject.subject_name}" updated'))
