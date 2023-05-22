f1ad2567fbd67aa5cb47218ac4cf441f9a8086ab
mariohuq <15021607+mariohuq@users.noreply.github.com>
2023-05-22 22:21:15 +0300

    WIP: add subject attendance table

---
+++ front/src/lib/SubjectTable.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let subjectId;
+    export let groupId;
+    export let subjectName;
+    export let groupName;
+    export let lessons;
+</script>
+
+<table>
+    <thead>
+        <tr>
+            {#each lessons[0].attendance_list as {abbrev_name, id, is_foreign, group_id} (id)}
+                <td class:foreign={is_foreign}>{abbrev_name}</td>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+    {#each lessons as {lesson_id, lesson_start_date, attendance_list} (lesson_id)}
+        <tr>
+            <th>{lesson_start_date}</th>
+            {#each attendance_list as {id, status} (id)}
+                <td>{status}</td>
+            {/each}
+        </tr>
+    {/each}
+    </tbody>
+</table>
+
+<style>
+    table {
+        display: table;
+    }
+    table tr {
+        display: table-cell;
+    }
+    table tr td {
+        display: block;
+    }
+
+    .foreign::after {
+        content: '*'
+    }
+</style>
---
+++ front/src/routes/subjects/[subject_id]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import SubjectTable from "$lib/SubjectTable.svelte";
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+<SubjectTable groupId={data.group.group_id},
+              groupName={data.group.groupname},
+              subjectId={data.subject.subject_id},
+              subjectName={data.subject.subject_name},
+              lessons={data.lessons}/>
