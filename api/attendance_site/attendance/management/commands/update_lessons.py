7c1df5022f00f278bfa9cb3d6bd36b688dd54b72
Александр <timofeev2.as@edu.spbstu.ru>
2023-05-23 00:06:32 +0300

    [] many changes: initialize db and update lessons will depends on date update subjects - neednt to use all funcs migrate to lessons upd
    amin'

--- api/attendance_site/attendance/management/commands/update_lessons.py
+++ api/attendance_site/attendance/management/commands/update_lessons.py
@@ -1,36 +1,79 @@
 from django.core.management.base import BaseCommand
 from attendance.models import Subject, Group, Teacher, Lesson
 from attendance.auxiliary.fill_lessons import get_lesson_by_group
-from datetime import datetime
+from attendance.auxiliary.find_teacher_by_id import get_teacher_by_id
+
+from datetime import datetime, timedelta
 import pytz
+
+from attendance.auxiliary.get_start_of_week_date import get_start_of_week_date
+from .make_new_user import make_new_user
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
+    created_count_lessons = 0
+    updated_count_lessons = 0
+    created_count_subjects = 0
+    updated_count_subjects = 0
+    created_count_teachers = 0
+    updated_count_teachers = 0
 
-
-        tz = pytz.timezone('Europe/Moscow')
-        # Получить данные из стороннего API
-        changes_count = 0
-        groups = Group.objects.all()
-        group = Group.objects.get(groupname='3530201/10001')
-        changes_count = 0
-        groups = Group.objects.all()
-        for group in groups:
-
-            items = get_lesson_by_group(group.group_id)
+    groups = Group.objects.all()
+    tz = pytz.timezone('Europe/Moscow')
+    def write_lesson_by_date(self,date_arg:str):
+        for group in self.groups:
+            # Получить данные из стороннего API
+            items = get_lesson_by_group(group.group_id,date_arg)
 
             # Добавление предметов в базу данных
-            subject_manager = Subject.objects
             lesson_manager = Lesson.objects
+            subject_manager = Subject.objects
 
+# найден баг с тем что если например есть лабы у разных лабщиков и это как две разные пары то не будет занесен препод поэтому тут я оставлю это сообщение чтобы это было
+# я сделаю try excep чтобы прога не падала аминь
             for item in items:
+
+                try:
+                    teacher = Teacher.objects.get(teacher_id=item['teacher_id'])
+                except Teacher.DoesNotExist:
+                    teacher_data = get_teacher_by_id(item['teacher_id'])
+                    teacher_user = make_new_user(teacher_data['first_name'])
+                    Teacher.objects.update_or_create(
+                        teacher_id=teacher_data['id'],
+                        user=teacher_user,
+                        teacher_name=teacher_data['full_name'],
+                    )
+
+                    if created:
+                        self.stdout.write(self.style.SUCCESS(f'Teacher with name "{teacher_data["full_name"]}" created'))
+                        self.created_count_teachers += 1
+                    else:
+                        self.stdout.write(
+                            self.style.SUCCESS(f'Teacher with name "{teacher_data["full_name"]}" updated'))
+                        self.updated_count_teachers += 1
+
                 teacher = Teacher.objects.get(teacher_id=item['teacher_id'])
+                try:
+                    subject = Subject.objects.get(subject_name=item['subject_name'], group__group_id=group.group_id,
+                                                teacher=teacher)
+                except Subject.DoesNotExist:
+                    subject, created = subject_manager.update_or_create(
+                        group=group,
+                        teacher=teacher,
+                        subject_name=item['subject_name']
+                    )
+
+                    if created:
+                        self.stdout.write(self.style.SUCCESS(f'Subject "{subject.subject_name}" created'))
+                        self.created_count_subjects += 1
+                    else:
+                        self.stdout.write(self.style.SUCCESS(f'Subject "{subject.subject_name}" updated'))
+                        self.updated_count_subjects += 1
+
                 subject = Subject.objects.get(subject_name=item['subject_name'], group__group_id=group.group_id,
                                               teacher=teacher)
-
-                lesson_start_time = tz.localize(datetime.strptime(item['lesson_start_time'], '%Y-%m-%d %H:%M:%S'))
-                lesson_end_time = tz.localize(datetime.strptime(item['lesson_end_time'], '%Y-%m-%d %H:%M:%S'))
+                lesson_start_time = self.tz.localize(datetime.strptime(item['lesson_start_time'], '%Y-%m-%d %H:%M:%S'))
+                lesson_end_time = self.tz.localize(datetime.strptime(item['lesson_end_time'], '%Y-%m-%d %H:%M:%S'))
                 print(lesson_start_time)
                 lesson, created = lesson_manager.update_or_create(
                     subject=subject,
@@ -40,8 +83,40 @@ class Command(BaseCommand):
 
                 if created:
                     self.stdout.write(self.style.SUCCESS(f'Lesson "{lesson.subject.subject_name}" created'))
-
+                    self.created_count_lessons += 1
                 else:
-                    self.stdout.write(self.style.SUCCESS(f'Subject "{lesson.subject.subject_name}" updated'))
-                changes_count += 1
-            self.stdout.write(self.style.SUCCESS(f'Lessons({changes_count}) updated or created'))
+                    self.stdout.write(self.style.SUCCESS(f'Lesson "{lesson.subject.subject_name}" updated'))
+                    self.updated_count_lessons += 1
+
+
+    def add_arguments(self, parser):
+        parser.add_argument('start_date', type=str, help='Start date in YYYY-MM-DD format')
+    def handle(self, *args, **options):
+        start_date_str = get_start_of_week_date(options['start_date'])
+        self.stdout.write(f"Start date: {start_date_str}")
+
+        start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+        delta = timedelta(days=7)
+        index_date = start_date
+
+        groups = Group.objects.all()
+        group = Group.objects.get(groupname='3530201/10001')
+        while index_date <= datetime.now():
+            date_arg = index_date.strftime("%Y-%m-%d")
+
+            self.stdout.write(f"\n\nStart date: {date_arg}\n\n")
+            self.write_lesson_by_date(date_arg)
+
+            index_date += delta
+
+        self.stdout.write(self.style.SUCCESS(f'Lessons updated {self.updated_count_lessons}'))
+        self.stdout.write(self.style.SUCCESS(f'Lessons created {self.created_count_lessons}'))
+
+        self.stdout.write(self.style.SUCCESS(f'Subjects updated {self.updated_count_subjects}'))
+        self.stdout.write(self.style.SUCCESS(f'Subjects created {self.created_count_subjects}'))
+
+
+        self.stdout.write(self.style.SUCCESS(f'Teachers updated {self.updated_count_teachers}'))
+        self.stdout.write(self.style.SUCCESS(f'Teachers created {self.created_count_teachers}'))
+
+
--- api/attendance_site/attendance/management/commands/update_subjects.py
+++ api/attendance_site/attendance/management/commands/update_subjects.py
@@ -1,27 +1,24 @@
 import json
+from datetime import datetime, timedelta
 import requests
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from attendance.models import Subject, Group, Teacher
 from attendance.auxiliary.fill_subjects import get_subject_week
 from attendance.auxiliary.find_teacher_by_id import get_teacher_by_id
-#from attendance.auxiliary.get_start_of_week_date import get_start_of_week_date
+from attendance.auxiliary.get_start_of_week_date import get_start_of_week_date
 from .make_new_user import make_new_user
 
 
 class Command(BaseCommand):
-    #def add_arguments(self, parser):
-    #    parser.add_argument('start_date', type=str, help='Start date in YYYY-MM-DD format')
-    def handle(self, *args, **options):
-        #start_date_str = get_start_of_week_date(options['start_date'])
-        #self.stdout.write(f"Start date: {start_date_str}")
+    created_count = 0
+    updated_count = 0
+    groups = Group.objects.all()
 
-
-        changes_count = 0
-        groups = Group.objects.all()
-        for group in groups:
+    def write_subject_by_date(self, date_arg: str):
+        for group in self.groups:
             # Получить данные из стороннего API
-            items = get_subject_week(group.group_id,)
+            items = get_subject_week(group.group_id, date_arg)
 
             # Добавление предметов в базу данных
             subject_manager = Subject.objects
@@ -51,8 +48,29 @@ class Command(BaseCommand):
 
                 if created:
                     self.stdout.write(self.style.SUCCESS(f'Subject "{subject.subject_name}" created'))
-                    changes_count+=1
+                    self.created_count += 1
                 else:
                     self.stdout.write(self.style.SUCCESS(f'Subject "{subject.subject_name}" updated'))
-                    changes_count+=1
-        self.stdout.write(self.style.SUCCESS(f'Subjects({changes_count}) updated or created'))
+                    self.updated_count += 1
+
+    def add_arguments(self, parser):
+        parser.add_argument('start_date', type=str, help='Start date in YYYY-MM-DD format')
+
+    def handle(self, *args, **options):
+        start_date_str = get_start_of_week_date(options['start_date'])
+        self.stdout.write(f"Start date: {start_date_str}")
+
+        start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+        delta = timedelta(days=7)
+        index_date = start_date
+
+        while index_date <= datetime.now():
+            date_arg = index_date.strftime("%Y-%m-%d")
+
+            self.stdout.write(f"\n\nStart date: {date_arg}\n\n")
+            self.write_subject_by_date(date_arg)
+
+            index_date += delta
+
+        self.stdout.write(self.style.SUCCESS(f'Subjects updated {self.updated_count}'))
+        self.stdout.write(self.style.SUCCESS(f'Subjects created {self.created_count}'))
