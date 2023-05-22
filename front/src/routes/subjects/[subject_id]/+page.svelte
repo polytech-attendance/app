c873b167c303d099d422f2b5ceb3df2d85f97785
mariohuq <15021607+mariohuq@users.noreply.github.com>
2023-05-23 01:52:14 +0300

    add title

--- front/src/routes/subjects/[subject_id]/+page.svelte
+++ front/src/routes/subjects/[subject_id]/+page.svelte
@@ -4,6 +4,10 @@
     export let data;
 </script>
 
+<h1>
+    Посещаемость группой {data.group.name} дисциплины {data.subject.name}
+</h1>
+
 <SubjectTable groupId={data.group.id},
               groupName={data.group.name},
               subjectId={data.subject.id},
