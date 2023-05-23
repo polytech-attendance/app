88f2e2a5f9c07418c6ab265b66a350f4cbd0f97b
mariohuq <15021607+mariohuq@users.noreply.github.com>
2023-05-23 13:24:15 +0300

    add table stats

---
+++ front/src/routes/subjects/[subject_id]/stats/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import StatsTable from "$lib/StatsTable.svelte";
+
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+<h1>
+    Статистика посещений группой {data.group.name} дисциплины {data.subject.name}
+</h1>
+
+<StatsTable studentList={data.group.student_list}/>
